298c8880343182cf5ee5b30cb7fbd93f8fda7464
ben-sandison <sandos@hotmail.co.uk>
2015-01-20 21:05:19 +0000

    Updated to draw hex grid + loads bitmap files in the format required for texturing in OpenGL.

---
+++ Hexatories/Hexatories/Textures.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Textures.h"
+
+unsigned char *loadBMP(const char *imagepath, unsigned int &width, unsigned int &height) {
+
+	// Data read from the header of the BMP file
+	unsigned char header[54];
+	unsigned int dataPos;
+	unsigned int imageSize;
+
+	// Actual RGB data
+	unsigned char * data;
+
+	FILE * file;
+	fopen_s(&file, imagepath, "rb");
+	if (!file) return NULL;
+
+	// Read the header
+	// If less than 54 byes are read, problem
+	if (fread(header, 1, 54, file) != 54) return NULL;
+
+	// A BMP files always begins with "BM"
+	if (header[0] != 'B' || header[1] != 'M') printf("Not a correct BMP file\n");
+
+	// Make sure this is a 24bpp file
+	if (*(int*)&(header[0x1E]) != 0) return NULL;
+	if (*(int*)&(header[0x1C]) != 24) return NULL;
+
+	dataPos = *(int*)&(header[0x0A]);
+	imageSize = *(int*)&(header[0x22]);
+	width = *(int*)&(header[0x12]);
+	height = *(int*)&(header[0x16]);
+
+	// If corrupt BMP
+	if (imageSize == 0) imageSize = width * height * 3; // 3 : one byte for each Red, Green and Blue component
+	if (dataPos == 0) dataPos = 54; // The BMP header is done that way
+
+	data = new unsigned char[imageSize];
+
+	// Read pixels from the file into the buffer
+	fread(data, 1, imageSize, file);
+
+	fclose(file);
+
+	return data;
+}
--- Hexatories/Hexatories/main.cpp
+++ Hexatories/Hexatories/main.cpp
@@ -1,101 +1,158 @@
 #include <GL\glew.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
+#include "Textures.h"
 
 //this is a test 5/3
 // (C) Group 16
 
-class Triangle{
-public:
-	GLuint vaoid;
-	GLuint vboid;
+class Hex {
 
-
-	int initSquare(){
+	GLuint vaoid, vboid;
+	
+	public:
+	int initHex(int x, int y) {
 		glGenVertexArrays(1, &vaoid);
 		glBindVertexArray(vaoid);
-		//Tells OpenGL that we are going to be using the vaoid array//
+		// Tells OpenGL that we are going to be using the vaoid array
 
-		static const GLfloat verts[] = {
-			//X, Y, Z
-			-0.5f, -0.5f, 0.0f,
-			0.5f, -0.5f, 0.0f,
-			0.0f, 0.5f, 0.0f
-
+		GLfloat verts[] = {
+			-0.98125f, 0.964375f,		// left
+			-0.96875f, 0.940625f,		// bottom left
+			-0.94375f, 0.940625f,		// bottom right
+			-0.93125f, 0.964375f,		// right
+			-0.94375f, 0.988125f,		// top right
+			-0.96875f, 0.988125f,		// top left
 		};
-		//This is an array of vertificies
-		//Contains the array for the coordinates for the triange
 
+		int xoff = x % 2;
+
+		for (int i = 0; i < 14; i += 2) {
+			verts[i] += 0.0375f * x;
+			verts[i + 1] -= 0.0475f * y + 0.02375f * xoff;
+		}
 
 		glGenBuffers(1, &vboid);
 		glBindBuffer(GL_ARRAY_BUFFER, vboid);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
 
 		return 0;
-
 	}
-	int drawSquare(){
+
+	int drawHex() {
+
 		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, vboid);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		short indices[] = { 0, 1, 2, 3, 4, 5};
 
+		glDrawElements(GL_POLYGON, 6, GL_UNSIGNED_SHORT, indices);
 
 		glDisableVertexAttribArray(0);
 
 		return 0;
 	}
-
 };
 
+void _update_fps_counter(GLFWwindow* window) {
 
+	static double previous_seconds = glfwGetTime();
+	static int frame_count;
+	double current_seconds = glfwGetTime();
+	double elapsed_seconds = current_seconds - previous_seconds;
 
-int main(void)
-{
-	GLFWwindow* window;
+	if (elapsed_seconds > 0.25) {
 
-	/* Initialize the library */
+		previous_seconds = current_seconds;
+		double fps = (double)frame_count / elapsed_seconds;
+		char tmp[128];
+
+		GLint m_viewport[4];
+		glGetIntegerv(GL_VIEWPORT, m_viewport);
+
+		sprintf_s(tmp, "Hexatories @ fps: %.2f", fps);
+		glfwSetWindowTitle(window, tmp);
+		frame_count = 0;
+	}
+
+	frame_count++;
+
+}
+
+GLFWwindow* initWindow(void) {
+
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow* window = NULL;
+
+	const GLFWvidmode * mode = glfwGetVideoMode(glfwGetPrimaryMonitor());	// Get screen resolution
+	int window_width = mode->width;
+	int window_height = mode->height;
+
+	if (window_width >= 1024 || window_height >= 768) {		// Ensures resolution is big enough and if necessary makes window fullscreen
+
+		if (window_width == 1024 || window_height == 768) {
+			window = glfwCreateWindow(1024, 768, "Hexatories", glfwGetPrimaryMonitor(), NULL);
+		} else {
+			window = glfwCreateWindow(1024, 768, "Hexatories", NULL, NULL);
+		}
+	}
+
+	return window;
+}
+
+int main(void) {
+
+	// Initialize the library 
 	if (!glfwInit())
 		return -1;
 
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Group 16 OpenGL Experiment", NULL, NULL);
-	if (!window)
-	{
+	GLFWwindow* window = initWindow();
+
+	if (!window) {
 		glfwTerminate();
 		return -1;
 	}
 
-	/* Make the window's context current */
+	// Make the window's context current
 	glfwMakeContextCurrent(window);
 
 	glewExperimental = true;
 
-	if (glewInit() != GLEW_OK){
+	if (glewInit() != GLEW_OK) {
 		glfwTerminate();
 		fprintf(stderr, "GLEW Failed to initalise");
 		return -1;
 	}
 
+	glViewport(142, 0, 740, 740);	// Basically changes the section of the window we draw to
 
+	Hex myHex[52][41];
+	for (int x = 0; x < 52; x++) {
+		for (int y = 0; y < 41; y++) {
+			myHex[x][y].initHex(x, y);
+		}
+	}
 
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-	Triangle myTriange;
-	myTriange.initSquare();
-
-
-	/* Loop until the user closes the window */
-
-
+	// Loop until the user closes the window
 	while (!glfwWindowShouldClose(window))
 	{
-		/* Render here */
-		myTriange.drawSquare();
-		/* Swap front and back buffers */
+		_update_fps_counter(window);
+
+		// Render here 
+		for (int x = 0; x < 52; x++) {
+			for (int y = 0; y < 41; y++) {
+				myHex[x][y].drawHex();
+			}
+		}
+
+		// Swap front and back buffers 
 		glfwSwapBuffers(window);
 
-		/* Poll for and process events */
+		// Poll for and process events 
 		glfwPollEvents();
 	}
 
