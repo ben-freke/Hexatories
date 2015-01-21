5270fd7091d193c223dea65b61d383d39a6f3298
ben-sandison <sandos@hotmail.co.uk>
2015-01-21 17:13:46 +0000

    Actually included the shaders to test this time >.<

---
+++ Hexatories/Hexatories/Shaders/test_fs.glsl
@@ -0,0 +1,5 @@
+#version 420
+out vec4 fragColour;
+void main() {
+	fragColour =  vec4 (0.2, 0.0, 0.2, 1.0);
+}
---
+++ Hexatories/Hexatories/Shaders/test_vs.glsl
@@ -0,0 +1,5 @@
+#version 420
+in vec2 vertex_position;
+void main() {
+	gl_Position = vec4(vertex_position, 0.0, 1.0);
+}
