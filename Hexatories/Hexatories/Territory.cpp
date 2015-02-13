bca6ef826a2e62a180a0df42c004690a30476c2d
ben-freke <b.freke@lancaster.ac.uk>
2015-02-13 17:49:14 +0000

    Game Attacks

--- Hexatories/Hexatories/Territory.cpp
+++ Hexatories/Hexatories/Territory.cpp
@@ -1,10 +1,31 @@
 #include "Territory.h"
 using namespace std;
 
+
+
 void Territory::initTerritory(vector<int> innerTiles, int tileCount) {
 
 	size = tileCount;
 	tiles = innerTiles;
+	
+
+	
 
 	//setupBorderTiles();
+}
+
+int Territory::getAttackScore(){
+	return troopsAttack;
+}
+
+void Territory::setAttackScore(int value){
+	troopsAttack = value;
+}
+
+int Territory::getDefenseScore(){
+	return troopsDefense;
+}
+
+void Territory::setDefenseScore(int value){
+	troopsDefense =  value;
 }
--- Hexatories/Hexatories/Territory.h
+++ Hexatories/Hexatories/Territory.h
@@ -9,10 +9,20 @@ class Territory {
 
 	std::vector<int> tiles, border;
 	int size;
+	int populationNo, troopsAttack, troopsDefense, territoryID, owner = 0;
+	bool farmBuilt, bankBuilt = false;
 
 public:
 
 	void initTerritory(std::vector<int>, int);
+
+	int getAttackScore();
+
+	void setAttackScore(int value);
+
+	int getDefenseScore();
+
+	void setDefenseScore(int value);
 
 	void setupBorderTiles();
 
