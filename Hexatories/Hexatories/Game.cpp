42ed432186006ecb40149807c57beb55155148ec
ben-freke <b.freke@lancaster.ac.uk>
2015-02-23 17:19:44 +0000

    Building checks gold and removes gold value

--- Hexatories/Hexatories/Game.cpp
+++ Hexatories/Hexatories/Game.cpp
@@ -11,6 +11,8 @@ using namespace std;
 
 void Game::initGame() {
 	newGame();
+	players[0].coins = 1500;
+	players[1].coins = 1500;
 }
 
 #pragma region save
@@ -327,12 +329,18 @@ void Game::handleMouseInput(double x, double y, bool click, bool reset) {
 	}
 
 	case gameUI::Section::BUY_FARM: {
-		map.updateBuilding(currTerr, true);	//Adds a farm to the territory & map
+		if (players[1].coins > 1500){
+			map.updateBuilding(currTerr, true);	//Adds a farm to the territory & map
+			players[1].coins = players[1].coins - 1500;
+		}
 		break;
 	}
 
 	case gameUI::Section::BUY_BANK: {
-		map.updateBuilding(currTerr, false);	//Adds a bank to the territory & map
+		if (players[1].coins > 1500){
+			map.updateBuilding(currTerr, false);	//Adds a bank to the territory & map
+			players[1].coins = players[1].coins - 1500;
+		}
 		break;
 	}
 
